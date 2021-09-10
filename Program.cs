bd26c556884c83b9d17d616f29672ce9e42cfafe
Alexia Raphaela <alexiaraphaela@gmail.com>
2021-09-10 12:08:31 -0300

    feat: first C# program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// qual grupo de ferramentas estou utilizando 
+// pacote proprio do dotnet
+// importando do dotnet
+using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        // metodo construtor da classe program
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for(int i=0; i < numeroDeVezes; i++)
+            // $ funcionalidade de c#
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
