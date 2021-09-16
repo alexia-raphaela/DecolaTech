fac468843a40cf5e19d1bd12b691ab9ae1192f6f
Alexia Raphaela <alexiaraphaela@gmail.com>
2021-09-16 11:56:23 -0300

    feat: Enum concept and dotnet review

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -7,6 +7,8 @@ namespace Revisao
         static void Main(string[] args)
         {
             Aluno[] alunos = new Aluno[5];
+            // ele assumi essa variavel como um iteiro 
+            var indiceAluno = 0;
             string opcaoUsuario = ObterOpcaoUsuario();
 
             // toUpper, mesmo que o usuario informe um x minusculo, transforma em maiuscula para ser feita a comparação
@@ -24,13 +26,71 @@ namespace Revisao
                         Console.WriteLine("informe a nota do aluno:");
 
                         // var é uma  declaracao de variavel sem tipo, que pega o tipo que o proprio valor representa
-                        var nota = decimal.Parse(Console.ReadLine());
+                        // var nota = decimal.Parse(Console.ReadLine());
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota)) 
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                      
                         break;
                     case "2":
-                        //TODO: listar alunos
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"AlUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
                         break;
                     case "3":
-                        //TODO: calcular media geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        // conceito é o meu enum
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral} ");
+
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
